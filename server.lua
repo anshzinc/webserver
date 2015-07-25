01942b5f4655952f204407806587b7fefcfe25d4
anshulv <anshzinc@gmail.com>
2015-07-25 22:19:55 +0530

    Add tiny url mapper

--- server.lua
+++ server.lua
@@ -30,13 +30,34 @@ function webserver:run_server()
 
 		if not err then
 			print("[Request] type: " .. req_parser:get_type(line) .. " url: " .. req_parser:get_url(line))
-			local resp_file = io.open("index.html", 'r')	
-			client:send(headers:get_200()  .. " " .. resp_file:read("*all") .. "\n") 
-			resp_file:close()
+			local resp_file = io.open(self:map_url(req_parser:get_url(line)), 'r')	
+
+			if resp_file then		
+
+				self:map_url(req_parser:get_url(line))
+
+				client:send(headers:get_200()  .. " " .. resp_file:read("*all") .. "\n") 
+				resp_file:close()
+			else
+				client:send(headers:get_404() .. " " .. "<h1>404 Not Found</h1>\n")
+			end
 		end
 	
 		client:close()
 	end
+end
+
+-- Router
+
+function webserver:map_url(url) 
+	local req_dirs = req_parser:split(url, "/")
+	local base_dir = "./" -- for now let base dir to be same directory
+	for _, v in ipairs(req_dirs) do
+		base_dir = base_dir .. "/" .. v
+	end
+
+	file = base_dir .. "/index.html"
+	return file
 end
 
 local server = webserver:init_server()
